561e1032a8203b4c95358adb6a0dfcaa3127ad61
AmanCH3 <armanachaudhary53@gmail.com>
2024-05-13 19:26:07 +0545

    feat : In Book Entity Lazy into cascade (Book projection implementation)

--- src/main/java/com/example/demox/entity/Book.java
+++ src/main/java/com/example/demox/entity/Book.java
@@ -1,12 +1,13 @@
 package com.example.demox.entity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name="books")
-@Setter
 @Getter
+@Setter
+@AllArgsConstructor
 
 public class Book {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "books_setup_seq_gen")
@@ -14,18 +15,22 @@ public class Book {
     @Id
     private Integer id;
 
-    @Column(name = "bookName",nullable = false,length = 100)
+    @Column(name = "bookName", nullable = false, length = 100)
     private String bookName;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ground_id", referencedColumnName = "id",
             foreignKey = @ForeignKey(name = "pk_id_fk_ground_id"))
-    private com.example.demox.entity.Ground ground;
+    private Ground ground;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id", referencedColumnName = "id",
             foreignKey = @ForeignKey(name = "pk_id_fk_user_id"))
     private User user;
+
+    public Book() {
+
+    }
 
     public void setName(String bookName) {
     }
--- src/main/java/com/example/demox/projection/BookProjection.java
+++ src/main/java/com/example/demox/projection/BookProjection.java
@@ -5,4 +5,5 @@ public interface BookProjection {
     String getBookName() ;
     String getGroundID() ;
     Integer getUserID();
+
 }
